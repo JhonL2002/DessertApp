07debcad67e7587729d739ed590ae4d5921fcb65
JhonL2002 <jhonfredylopezarenas@gmail.com>
2025-01-08 14:34:58 -0500

    change: Changed viewmodels from "DessertApp.Models" to "DessertApp (web project)"
    - Deleted unused references to perform a good implementation of changes.

--- DessertApp.Infraestructure/Data/AppDbContext.cs
+++ DessertApp.Infraestructure/Data/AppDbContext.cs
@@ -1,12 +1,6 @@
 ﻿using DessertApp.Infraestructure.IdentityModels;
-using DessertApp.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DessertApp.Infraestructure.Data
 {
