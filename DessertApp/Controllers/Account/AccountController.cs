9ad643de80ffd02601f2fbcfc2d7c8f6bb32e1cc
JhonL2002 <jhonfredylopezarenas@gmail.com>
2025-01-08 14:26:11 -0500

    feat: Created "AccountController" to perform Login operations in system app.
    - Deleted "Login" page using in "Areas" to use views.
    - Created "LoginVM" to get data from formulary to do Login process.
    - Created "Login" view to interact with data controller.
    - Updated "_LoginPartial.cshtml" to verify the new routes

---
+++ DessertApp/Controllers/Account/AccountController.cs
@@ -0,0 +1,58 @@
+﻿using DessertApp.Services.AccountServices;
+using DessertApp.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DessertApp.Controllers.Account
+{
+    public class AccountController : Controller
+    {
+        private readonly IAuthenticationService<Microsoft.AspNetCore.Identity.SignInResult, IdentityResult> _authenticationService;
+        private readonly ILogger<AccountController> _logger;
+
+        public AccountController(
+            IAuthenticationService<Microsoft.AspNetCore.Identity.SignInResult, IdentityResult> authenticationService,
+            ILogger<AccountController> logger)
+        {
+            _authenticationService = authenticationService;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Login(string returnUrl = null!)
+        {
+            //Pass returnUrl to view to handle post-login redirection
+            ViewData["ReturnUrl"] = returnUrl ?? Url.Content("~/");
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginVM login, string returnUrl = null!)
+        {
+            returnUrl ??= Url.Content("~/");
+            if (ModelState.IsValid)
+            {
+                var result = await _authenticationService.SignInAsync(login.Email!,login.Password!, false, false);
+                if (result.Succeeded)
+                {
+                    _logger.LogInformation("User logged in.");
+                    return LocalRedirect(returnUrl);
+                }
+                if (result.IsLockedOut)
+                {
+                    _logger.LogWarning("User account locked out.");
+                    return RedirectToPage("/Account/Lockout");
+                }
+                else
+                {
+                    ModelState.AddModelError(string.Empty, "Invalid login attempt, verify data");
+                }
+            }
+
+            // Redisplay form with validation errors and input
+            ViewData["ReturnUrl"] = returnUrl;
+            return View(login);
+        }
+    }
+}
---
+++ DessertApp/ViewModels/LoginVM.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace DessertApp.ViewModels
+{
+    public class LoginVM
+    {
+        [Required]
+        [EmailAddress]
+        public string? Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string? Password { get; set; }
+    }
+}
