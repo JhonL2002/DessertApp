b2c0a165f6b39f409c35f27f3b7f909d1cedf2ea
jhonl2002 <jhonfredylopezarenas@gmail.com>
2025-01-22 19:17:54 -0500

    change: Changed "Ingredient" and "IngredientUnit" to perform one-to-one relationship

--- DessertApp.Models/Entities/Ingredient.cs
+++ DessertApp.Models/Entities/Ingredient.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace DessertApp.Models.Entities
 {
@@ -21,33 +15,6 @@ namespace DessertApp.Models.Entities
 
         public bool IsAvailable { get; set; }
 
-        [Range(0.0, double.MaxValue)]
-        public decimal? CostPerUnit { get; set; }
-
-        [Range(0.0, double.MaxValue)]
-        public decimal? OrderingCost {  get; set; }
-
-        [Range(0.0, 1.0)]
-        public decimal? MonthlyHoldingCostRate {  get; set; }
-
-        [NotMapped]
-        //Calculated property to ensure best analysis
-        public decimal AnnualHoldingCost => CostPerUnit.HasValue && MonthlyHoldingCostRate.HasValue
-            ? CostPerUnit.Value * MonthlyHoldingCostRate.Value * 12
-            : 0;
-
-        public int? AnnualDemand {  get; set; }
-
-        [NotMapped]
-        //Calculated property for EOQ
-        public int EconomicOrderQuantity =>
-            AnnualDemand.HasValue && AnnualDemand > 0 && OrderingCost.HasValue && OrderingCost > 0 && AnnualHoldingCost > 0
-                ? (int)Math.Sqrt((2 * AnnualDemand.Value * (double)OrderingCost.Value) / (double)AnnualHoldingCost)
-                : 0;
-
-        [NotMapped]
-        public decimal PeriodicOrderQuantity => EconomicOrderQuantity > 0 && AnnualDemand.HasValue
-            ? EconomicOrderQuantity / (AnnualDemand.Value / 12m)
-            : 0;
+        public IngredientUnit? IngredientUnit { get; set; }
     }
 }
---
+++ DessertApp.Models/Entities/IngredientUnit.cs
@@ -0,0 +1,48 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DessertApp.Models.Entities
+{
+    public class IngredientUnit
+    {
+        public int Id { get; set; }
+
+        public int IngredientId { get; set; }
+        public Ingredient? Ingredient { get; set; }
+
+        public int UnitId { get; set; }
+        public MeasurementUnit? Unit { get; set; }
+
+        //Optional additional details
+        [Range(0, int.MaxValue)]
+        public int ItemsPerUnit { get; set; } = 0; // Number of items in a box, bottle, package
+
+        [Range(0.0, double.MaxValue)]
+        public decimal? CostPerUnit { get; set; }
+
+        [Range(0.0, double.MaxValue)]
+        public decimal? OrderingCost { get; set; }
+
+        [Range(0.0, 1.0)]
+        public decimal? MonthlyHoldingCostRate { get; set; }
+
+        public int? AnnualDemand {  get; set; }
+
+        [NotMapped]
+        public decimal AnnualHoldingCost => CostPerUnit.HasValue && MonthlyHoldingCostRate.HasValue
+            ? CostPerUnit.Value * MonthlyHoldingCostRate.Value * 12
+            : 0;
+
+        [NotMapped]
+        public int EconomicOrderQuantity =>
+            AnnualDemand.HasValue && AnnualDemand > 0 && OrderingCost.HasValue && OrderingCost > 0 && AnnualHoldingCost > 0
+                ? (int)Math.Sqrt((2 * AnnualDemand.Value * (double)OrderingCost.Value) / (double)AnnualHoldingCost)
+                : 0;
+
+        [NotMapped]
+        public decimal PeriodicOrderQuantity => EconomicOrderQuantity > 0 && AnnualDemand.HasValue
+            ? EconomicOrderQuantity / (AnnualDemand.Value / 12m)
+            : 0;
+    }
+}
