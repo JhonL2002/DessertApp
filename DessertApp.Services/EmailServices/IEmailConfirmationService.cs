74750f1ccc7b0df8cf4450c2b9a3bc8d87e7ea39
jhonl2002 <jhonfredylopezarenas@gmail.com>
2025-01-24 16:59:05 -0500

    deletion: Deleted unnecessary archives or modified classes.

--- DessertApp.Services/EmailServices/IEmailConfirmationService.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace DessertApp.Services.EmailServices
-{
-    public interface IEmailConfirmationService<T> where T : class
-    {
-        Task<string> GenerateConfirmationUrlAsync(T user, string returnUrl);
-        Task SendConfirmationEmailAsync(string email, string confirmationUrl);
-    }
-}
