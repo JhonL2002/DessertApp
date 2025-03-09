55aec72fb98bf021ec54075bba46469e2b1a1598
jhonl2002 <jhonfredylopezarenas@gmail.com>
2025-03-09 15:49:45 -0500

    feat: Implemented remaining orders substraction into ApprovePurchaseOrderHandle to reduce the total remaining orders to do into system when it's approved

--- DessertApp.Application/Features/PurchaseOrders/Handlers/ApprovePurchaseOrderHandler.cs
+++ DessertApp.Application/Features/PurchaseOrders/Handlers/ApprovePurchaseOrderHandler.cs
@@ -9,22 +9,29 @@ namespace DessertApp.Application.Features.PurchaseOrders.Handlers
     public class ApprovePurchaseOrderHandler : IRequestHandler<ApprovePurchaseOrderCommand, bool>
     {
         private readonly IUnitOfWork _unitOfWork;
+        private readonly IPurchaseOrderRepository _purchaseOrderRepository;
 
-        public ApprovePurchaseOrderHandler(IUnitOfWork unitOfWork, IMediator mediator)
+        public ApprovePurchaseOrderHandler(IUnitOfWork unitOfWork, IMediator mediator, IPurchaseOrderRepository purchaseOrderRepository)
         {
             _unitOfWork = unitOfWork;
+            _purchaseOrderRepository = purchaseOrderRepository;
         }
 
         public async Task<bool> Handle(ApprovePurchaseOrderCommand request, CancellationToken cancellationToken)
         {
-            var order = await _unitOfWork.PurchaseOrders.GetByIdAsync(request.OrderId, cancellationToken);
+            var order = await _purchaseOrderRepository.GetOrderWithDetailsAsync(request.OrderId, cancellationToken);
+
             if (order == null || order.IsApproved)
                 return false;
+
+            foreach (var detail in order.OrderDetails)
+            {
+                detail.RemainingOrders = detail.RemainingOrders - 1;
+            }
 
             order.IsApproved = true;
             await _unitOfWork.PurchaseOrders.UpdateAsync(order, cancellationToken);
             await _unitOfWork.SaveChangesAsync(cancellationToken);
-
             return true;
         }
     }
